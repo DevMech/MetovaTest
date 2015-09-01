a628942efd3a2e1c8a6db6af4da6ead3d1183ff9
DevMech <agmecham@gmail.com>
2015-09-01 01:24:09 -0600

    Changed input

--- MetovaTest/AppDelegate.m
+++ MetovaTest/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    if ([self parensCheckFaster:@"()()()()()"]) {
+    if ([self parensCheck:@"()()()()()"]) {
         NSLog(@"It's a pair!");
     } else {
         NSLog(@"Somebody's closer");
